dea26a59cac77246d706a66b307d6e215774509b
abdalasamirg <106495890+abdalasamirg@users.noreply.github.com>
2022-10-01 21:03:20 +0200

    new-updat

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ let alpha = [" ", '1', '2', '3', '4', '5', '6', '7', '8']
 for (let l = 1; l < 9; l++) {
     let col1 = document.createElement("div")
     col1.className = "col-1"
-    col1.id = alpha[l] + a
+    col1.id = alpha[l] + "a"
+    console.log(col1.id)
     numcol1 = col1.id.match(/\d+/)[0]
     row1.appendChild(col1)
     let count = numcol1 % 2
@@ -37,35 +38,35 @@ for (let l = 1; l < 9; l++) {
         if (col1.id === count == 0)
             col1.style.backgroundColor = "gray"
     }
-    if (col1.id === "1[object HTMLDivElement]") {
+    if (col1.id === "1a") {
         col1.textContent = "♜"
 
     }
-    if (col1.id === "2[object HTMLDivElement]") {
+    if (col1.id === "2a") {
         col1.textContent = "♞"
 
     }
-    if (col1.id === "3[object HTMLDivElement]") {
+    if (col1.id === "3a") {
         col1.textContent = "♝"
 
     }
-    if (col1.id === "4[object HTMLDivElement]") {
+    if (col1.id === "4a") {
         col1.textContent = "♛"
 
     }
-    if (col1.id === "5[object HTMLDivElement]") {
+    if (col1.id === "5a") {
         col1.textContent = "♚"
 
     }
-    if (col1.id === "6[object HTMLDivElement]") {
+    if (col1.id === "6a") {
         col1.textContent = "♝"
 
     }
-    if (col1.id === "7[object HTMLDivElement]") {
+    if (col1.id === "7a") {
         col1.textContent = "♞"
 
     }
-    if (col1.id === "8[object HTMLDivElement]") {
+    if (col1.id === "8a") {
         col1.textContent = "♜"
 
     }
@@ -75,7 +76,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col2 = document.createElement("div")
     col2.className = "col-1"
-    col2.id = alpha[l] + a
+    col2.id = alpha[l] + "b"
     numcol2 = col2.id.match(/\d+/)[0]
     row2.appendChild(col2)
     let count = numcol2 % 2
@@ -84,11 +85,12 @@ for (let l = 1; l < 9; l++) {
             col2.style.backgroundColor = "wheat"
     }
     col2.textContent = "♟"
+
 }
 for (let l = 1; l < 9; l++) {
     let col3 = document.createElement("div")
     col3.className = "col-1"
-    col3.id = alpha[l] + a
+    col3.id = alpha[l] + "c"
     numcol3 = col3.id.match(/\d+/)[0]
     row3.appendChild(col3)
     let count = numcol3 % 2
@@ -100,7 +102,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col4 = document.createElement("div")
     col4.className = "col-1"
-    col4.id = alpha[l] + a
+    col4.id = alpha[l] + "d"
     numcol4 = col4.id.match(/\d+/)[0]
     row4.appendChild(col4)
     let count = numcol4 % 2
@@ -112,7 +114,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col5 = document.createElement("div")
     col5.className = "col-1"
-    col5.id = alpha[l] + a
+    col5.id = alpha[l] + "e"
     numcol5 = col5.id.match(/\d+/)[0]
     row5.appendChild(col5)
     let count = numcol5 % 2
@@ -124,7 +126,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col6 = document.createElement("div")
     col6.className = "col-1"
-    col6.id = alpha[l] + a
+    col6.id = alpha[l] + "f"
     numcol6 = col6.id.match(/\d+/)[0]
     row6.appendChild(col6)
     let count = numcol6 % 2
@@ -142,7 +144,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col7 = document.createElement("div")
     col7.className = "col-1"
-    col7.id = alpha[l] + a
+    col7.id = alpha[l] + "g"
     numcol7 = col7.id.match(/\d+/)[0]
     row7.appendChild(col7)
     let count = numcol7 % 2
@@ -159,7 +161,7 @@ for (let l = 1; l < 9; l++) {
 for (let l = 1; l < 9; l++) {
     let col8 = document.createElement("div")
     col8.className = "col-1"
-    col8.id = alpha[l] + a
+    col8.id = alpha[l] + "h"
     numcol8 = col8.id.match(/\d+/)[0]
     row8.appendChild(col8)
     let count = numcol8 % 2
@@ -167,43 +169,39 @@ for (let l = 1; l < 9; l++) {
         if (col8.id === count == 0)
             col8.style.backgroundColor = "wheat"
     }
-    if (col8.id === "1[object HTMLDivElement]") {
+    if (col8.id === "1h") {
         col8.textContent = "♖"
     }
-    if (col8.id === "2[object HTMLDivElement]") {
+    if (col8.id === "2h") {
         col8.textContent = "♘"
 
     }
-    if (col8.id === "3[object HTMLDivElement]") {
+    if (col8.id === "3h") {
         col8.textContent = "♗"
 
     }
-    if (col8.id === "4[object HTMLDivElement]") {
+    if (col8.id === "4h") {
         col8.textContent = "♕"
 
     }
-    if (col8.id === "5[object HTMLDivElement]") {
+    if (col8.id === "5h") {
         col8.textContent = "♔"
 
     }
-    if (col8.id === "6[object HTMLDivElement]") {
+    if (col8.id === "6h") {
         col8.textContent = "♗"
 
     }
-    if (col8.id === "7[object HTMLDivElement]") {
+    if (col8.id === "7h") {
         col8.textContent = "♘"
 
     }
-    if (col8.id === "8[object HTMLDivElement]") {
+    if (col8.id === "8h") {
         col8.textContent = "♖"
     }
 
 
 }
 setInterval(function() {
-    let r = document.getElementById("1[object HTMLDivElement]")
-    let r2 = document.getElementById("2[object HTMLDivElement]")
-    r.onclick = function() {
-        r2.textContent = "0"
-    }
+
 }, 1000);
